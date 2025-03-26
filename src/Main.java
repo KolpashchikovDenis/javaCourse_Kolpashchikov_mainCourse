aad9553b325c3488e02f023059ace340f393e70c
KolpashchikovaNatalia <nakolpash@mail.ru>
2025-03-26 16:04:45 +0400

    Add Main.java

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static String name = "Денис Викторович";
+
+    public static int age = 47;
+
+    public static void main(String[] args) {
+        System.out.printf("\nПривет, %s. Мне %d лет.\n", name, age);
+    }
+}
